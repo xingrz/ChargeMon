a797b0bbd217d1434165a12812d17762ee783bcf
XiNGRZ <hi@xingrz.me>
2021-06-03 23:41:00 +0800

    Wrap a-statistic as a new component with milli- support

---
+++ src/components/MilliData.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    type="link"
+    v-bind:style="{ cursor: 'pointer' }"
+    v-on:click="() => (this.milli = !this.milli)"
+  >
+    <a-statistic
+      v-bind:title="title"
+      v-bind:value="milli ? value * 1000 : value"
+      v-bind:suffix="milli ? suffixMilli : suffix"
+      v-bind:precision="milli ? precision - 3 : precision"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class MilliData extends Vue {
+  @Prop() title?: string;
+  @Prop() suffix?: string;
+  @Prop() suffixMilli?: string;
+  @Prop({ default: 0 }) value!: number;
+  @Prop({ default: 0 }) precision!: number;
+
+  milli = false;
+}
+</script>
--- src/components/RealtimeCard.vue
+++ src/components/RealtimeCard.vue
@@ -18,76 +18,65 @@
       <a-col v-bind:xs="24" v-bind:md="21">
         <a-row v-bind:gutter="[16, 16]" type="flex" align="middle">
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <div
-              type="link"
-              v-bind:style="{ cursor: 'pointer' }"
-              v-on:click="() => (this.mv = !this.mv)"
-            >
-              <a-statistic
-                title="电压"
-                v-bind:value="mv ? v * 1000 : v"
-                v-bind:suffix="mv ? 'mV' : 'V'"
-                v-bind:precision="mv ? 1 : 4"
-              />
-            </div>
-          </a-col>
-          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <div
-              type="link"
-              v-bind:style="{ cursor: 'pointer' }"
-              v-on:click="() => (this.ma = !this.ma)"
-            >
-              <a-statistic
-                title="电流"
-                v-bind:value="ma ? a * 1000 : a"
-                v-bind:suffix="ma ? 'mA' : 'A'"
-                v-bind:precision="ma ? 1 : 4"
-              />
-            </div>
-          </a-col>
-          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <div
-              type="link"
-              v-bind:style="{ cursor: 'pointer' }"
-              v-on:click="() => (this.mw = !this.mw)"
-            >
-              <a-statistic
-                title="功率"
-                v-bind:value="mw ? w * 1000 : w"
-                v-bind:suffix="mw ? 'mW' : 'W'"
-                v-bind:precision="mw ? 1 : 4"
-              />
-            </div>
-          </a-col>
-          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="容量"
-              v-bind:value="data ? data.ah : 0"
-              suffix="Ah"
-              v-bind:precision="4"
-            />
-          </a-col>
-          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="能量"
-              v-bind:value="data ? data.wh : 0"
-              suffix="Wh"
-              v-bind:precision="4"
-            />
-          </a-col>
-          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="D+"
-              v-bind:value="data ? data.dp : 0"
+            <MilliData
+              title="电压"
               suffix="V"
+              suffixMilli="mV"
+              v-bind:value="(data || {}).v"
+              v-bind:precision="4"
+            />
+          </a-col>
+          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
+            <MilliData
+              title="电流"
+              suffix="A"
+              suffixMilli="mA"
+              v-bind:value="(data || {}).a"
+              v-bind:precision="4"
+            />
+          </a-col>
+          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
+            <MilliData
+              title="功率"
+              suffix="W"
+              suffixMilli="mW"
+              v-bind:value="(data || {}).w"
+              v-bind:precision="4"
+            />
+          </a-col>
+          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
+            <MilliData
+              title="容量"
+              suffix="Ah"
+              suffixMilli="mAh"
+              v-bind:value="(data || {}).ah"
+              v-bind:precision="4"
+            />
+          </a-col>
+          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
+            <MilliData
+              title="能量"
+              suffix="Wh"
+              suffixMilli="mWh"
+              v-bind:value="(data || {}).wh"
+              v-bind:precision="4"
+            />
+          </a-col>
+          <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
+            <MilliData
+              title="D+"
+              suffix="V"
+              suffixMilli="mV"
+              v-bind:value="(data || {}).dp"
               v-bind:precision="3"
             />
           </a-col>
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
+            <MilliData
               title="D-"
-              v-bind:value="data ? data.dn : 0"
               suffix="V"
+              suffixMilli="mV"
+              v-bind:value="(data || {}).dn"
               v-bind:precision="3"
             />
           </a-col>
@@ -101,22 +90,16 @@
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { IYZXMessage } from "../YZX";
 
-@Component
+import MilliData from "./MilliData.vue";
+
+@Component({
+  components: {
+    MilliData,
+  },
+})
 export default class RealtimeCard extends Vue {
   @Prop() data?: IYZXMessage | null;
   @Prop() state?: "closed" | "opening" | "opened";
-
-  mv = false;
-  ma = false;
-  mw = false;
-
-  get v(): number {
-    return this.data ? this.data.v : 0;
-  }
-
-  get a(): number {
-    return this.data ? this.data.a : 0;
-  }
 
   get w(): number {
     return this.data ? this.data.a * this.data.v : 0;
