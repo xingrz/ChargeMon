42150523078deeac0804bf988334295d69280694
XiNGRZ <hi@xingrz.me>
2021-06-03 18:39:07 +0800

    Toggling between V/A/W and mV/mA/mW

--- src/components/RealtimeCard.vue
+++ src/components/RealtimeCard.vue
@@ -18,28 +18,46 @@
       <a-col v-bind:xs="24" v-bind:md="21">
         <a-row v-bind:gutter="[16, 16]" type="flex" align="middle">
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="电压"
-              v-bind:value="data ? data.v : 0"
-              suffix="V"
-              v-bind:precision="4"
-            />
+            <div
+              type="link"
+              v-bind:style="{ cursor: 'pointer' }"
+              v-on:click="() => (this.mv = !this.mv)"
+            >
+              <a-statistic
+                title="电压"
+                v-bind:value="mv ? v * 1000 : v"
+                v-bind:suffix="mv ? 'mV' : 'V'"
+                v-bind:precision="mv ? 1 : 4"
+              />
+            </div>
           </a-col>
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="电流"
-              v-bind:value="data ? data.a : 0"
-              suffix="A"
-              v-bind:precision="4"
-            />
+            <div
+              type="link"
+              v-bind:style="{ cursor: 'pointer' }"
+              v-on:click="() => (this.ma = !this.ma)"
+            >
+              <a-statistic
+                title="电流"
+                v-bind:value="ma ? a * 1000 : a"
+                v-bind:suffix="ma ? 'mA' : 'A'"
+                v-bind:precision="ma ? 1 : 4"
+              />
+            </div>
           </a-col>
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
-            <a-statistic
-              title="功率"
-              v-bind:value="data ? data.a * data.v : 0"
-              suffix="W"
-              v-bind:precision="4"
-            />
+            <div
+              type="link"
+              v-bind:style="{ cursor: 'pointer' }"
+              v-on:click="() => (this.mw = !this.mw)"
+            >
+              <a-statistic
+                title="功率"
+                v-bind:value="mw ? w * 1000 : w"
+                v-bind:suffix="mw ? 'mW' : 'W'"
+                v-bind:precision="mw ? 1 : 4"
+              />
+            </div>
           </a-col>
           <a-col v-bind:xs="12" v-bind:sm="8" v-bind:lg="3">
             <a-statistic
@@ -87,5 +105,21 @@ import { IYZXMessage } from "../YZX";
 export default class RealtimeCard extends Vue {
   @Prop() data?: IYZXMessage | null;
   @Prop() state?: "closed" | "opening" | "opened";
+
+  mv = false;
+  ma = false;
+  mw = false;
+
+  get v(): number {
+    return this.data ? this.data.v : 0;
+  }
+
+  get a(): number {
+    return this.data ? this.data.a : 0;
+  }
+
+  get w(): number {
+    return this.data ? this.data.a * this.data.v : 0;
+  }
 }
 </script>
